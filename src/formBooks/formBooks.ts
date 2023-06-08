69a530633c1417c9bc1aa702ebe48e779ced07b5
GLeonBS <gabriel.leonbs@gmail.com>
2023-06-08 15:18:24 -0300

    Adicionando paginas

--- src/formBooks/formBooks.ts
+++ src/formBooks/formBooks.ts
@@ -1,15 +1,32 @@
 export const formService = () => {
     const form = document.querySelector('form')!
-    
-    if(form){
-        const data = new FormData(form)
-        
-        console.log(data.entries());
+    const changeButton = document.querySelector<HTMLButtonElement>("#change")!
+    const bodyApplication = document.querySelector<HTMLDivElement>("#bodyApplication")!
+    let pagina = 0
+    const thisPage = window.location
+    console.log(changeButton.textContent);
+    changeButton.onclick = () => {
+        console.log(form);
+        if (pagina === 0) {
+            changeButton.innerText = "Cadastrar um livro"
+            bodyApplication.innerHTML = "Ola"
+            pagina = 1
+        }else{
+            thisPage.reload()
+        } 
+    }
 
+    if (form) {
         form.onsubmit = (ev) => {
+            ev.preventDefault()
+            const data = new FormData(form)
+
+            console.log(data.entries());
             ev.preventDefault()
 
             console.log(Object.fromEntries(data.entries()))
         }
     }
+
+
 }
