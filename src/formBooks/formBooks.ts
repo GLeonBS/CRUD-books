add8ee5bdf91f8390b6aed8bc6decd84499af02b
GLeonBS <gabriel.leonbs@gmail.com>
2023-06-08 19:10:53 -0300

    Update form

--- src/formBooks/formBooks.ts
+++ src/formBooks/formBooks.ts
@@ -4,26 +4,20 @@ export const formService = () => {
     const bodyApplication = document.querySelector<HTMLDivElement>("#bodyApplication")!
     let pagina = 0
     const thisPage = window.location
-    console.log(changeButton.textContent);
+
     changeButton.onclick = () => {
-        console.log(form);
         if (pagina === 0) {
             changeButton.innerText = "Cadastrar um livro"
-            bodyApplication.innerHTML = "Ola"
+            bodyApplication.innerHTML = "Você não tem um livro cadastrado :/"
             pagina = 1
-        }else{
+        } else {
             thisPage.reload()
-        } 
+        }
     }
-
     if (form) {
         form.onsubmit = (ev) => {
             ev.preventDefault()
             const data = new FormData(form)
-
-            console.log(data.entries());
-            ev.preventDefault()
-
             console.log(Object.fromEntries(data.entries()))
         }
     }
